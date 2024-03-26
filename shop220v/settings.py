db7413591aa84eabe09ca74190058706082e3daf
alex <akhristenko95@gmail.com>
2024-03-26 08:32:57 +0300

    second

--- shop220v/settings.py
+++ shop220v/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9-nwm^zyqmt&_u#)!rl+0(d@*_ut##kgp=cwe0bdsxj-ql2^3%'
+SECRET_KEY = 'django-insecure-5-nwm^zyqmt&_u#)!rl+0(d@*_ut##kgp=cwe0bdsxj-ql2^3%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
