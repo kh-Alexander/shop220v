4eb95f6fea42823ce9b59511d0a2a6308775a28f
alex <akhristenko95@gmail.com>
2024-03-26 21:25:08 +0300

    5

--- shop220v/settings.py
+++ shop220v/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-9-nwm^zyqmt&_u#)!rl+0(d@*_ut##kgp=cwe0bdsxj-ql2^3%'
-luy
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
